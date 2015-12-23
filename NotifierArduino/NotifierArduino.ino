3519c1383ec0fa8ad4283b9e83d9772b0c9c476c
Bruno de Lima e Silva <dlimaun@gmail.com>
2015-12-23 11:31:57 -0300

    Make arduino use serial bluetooth only

--- NotifierArduino/NotifierArduino.ino
+++ NotifierArduino/NotifierArduino.ino
@@ -1,7 +1,8 @@
-#include <SoftwareSerial.h> 
-#include <MeetAndroid.h>
+#include <SoftwareSerial.h>
+#include <stdlib.h>
 
-MeetAndroid meetAndroid(3,2,9600); // rx tx
+#define bluetooth_rx 3
+#define bluetooth_tx 2
 
 #define onboardLed 13
 
@@ -22,125 +23,105 @@ int nextRed;
 int nextGreen;
 int nextBlue;
 
-int count;
-
 int colorMod;
 boolean showColors;
 
-void setup()  
-{
-//  Serial.begin(9600); 
-  
-  pinMode(redLed, OUTPUT);
-  pinMode(greenLed, OUTPUT);
-  pinMode(blueLed, OUTPUT);
-  
-  meetAndroid.registerFunction(red, 'R'); 
-  meetAndroid.registerFunction(green, 'G'); 
-  meetAndroid.registerFunction(blue, 'B'); 
-  meetAndroid.registerFunction(timeTick, 'T'); 
-  
-  digitalWrite(onboardLed, LOW);
-  
-  check(redLed);
-  check(greenLed);
-  check(blueLed);
-}
+void setup() {
+    Serial.begin(9600); 
 
-void check(int color){
-  analogWrite(color, 150);
-  delay(500);
-  analogWrite(color, 0);
-}
+    pinMode(redLed, OUTPUT);
+    pinMode(greenLed, OUTPUT);
+    pinMode(blueLed, OUTPUT);
 
-void loop()
-{
-  checkBluetooth();
-  changeColors();
-  delay(mainLoopDelay);
-}
-
-void checkBluetooth(){
-  if (count == 0) 
-    meetAndroid.receive();
-  count = count + mainLoopDelay;
-  if (count > bluetoothDelay)
-    count = 0;
-}
-
-void changeColors(){
-  colorMod = colorMod + 1;
-  if (colorMod > maxColorMod){
-    showColors = !showColors;
-    colorMod = 0;
-  }
-  
-  if (showColors) {
-    redA    = updateColor(nextRed,   redA,   redLed);
-    greenA  = updateColor(nextGreen, greenA, greenLed);
-    blueA   = updateColor(nextBlue,  blueA,  blueLed);
-  } else {
-    redA    = updateColor(0,   redA,   redLed);
-    greenA  = updateColor(0, greenA, greenLed);
-    blueA   = updateColor(0,  blueA,  blueLed);
-  }
-}
-
-int updateColor(int nextColor, int actualColor, int ledPin){
-  if (nextColor > actualColor)
-    actualColor = actualColor + 1;
-  else if (nextColor < actualColor)
-    actualColor = actualColor - 1;
-  analogWrite(ledPin, actualColor);
-  return actualColor;
-}
-
-void red(byte flag, byte numOfValues)
-{
-  int intensity = meetAndroid.getInt();
-  nextRed = intensity;
-//  analogWrite(redLed, intensity);
-//  Serial.write("red = ");
-//  Serial.write(intensity);
-}
-
-void green(byte flag, byte numOfValues)
-{
-  int intensity = meetAndroid.getInt();
-  nextGreen = intensity;
-//  analogWrite(greenLed, intensity);
-//  Serial.write("green = ");
-//  Serial.write(intensity);
-}
-
-void blue(byte flag, byte numOfValues)
-{
-  int intensity = meetAndroid.getInt();
-  nextBlue = intensity;
-//  analogWrite(blueLed, intensity);
-//  Serial.write("blue = ");
-//  Serial.write(intensity);
-}
-
-void timeTick(byte flag, byte numOfValues)
-{
-  int minutes = meetAndroid.getInt();
-  
-  if (minutes == 0) 
-  {
     digitalWrite(onboardLed, LOW);
-    delay(1000);
-    digitalWrite(onboardLed, HIGH);
-  }
-  else 
-  {
-    for (int i=0; i<minutes; i++)
-    {
-      digitalWrite(onboardLed, LOW);
-      delay(75);
-      digitalWrite(onboardLed, HIGH);
-      delay(75);
-    }
-  }
+
+    check(redLed);
+    check(greenLed);
+    check(blueLed);
 }
 
+void check(int color) {
+    analogWrite(color, 150);
+    delay(1000);
+    analogWrite(color, 0);
+}
+
+void loop() {
+    checkBluetooth();
+    changeColors();
+    delay(mainLoopDelay);
+}
+
+void checkBluetooth() {
+    if (Serial.available() > 0) {
+            // read the incoming byte:
+            int incomingByte = Serial.read();
+            String value = Serial.readString();
+            
+            switch (incomingByte){
+              case 'r':
+                print("Red", value);
+                nextRed = value.toInt();
+              break;
+              case 'g':
+                print("Green", value);
+                nextGreen = value.toInt();
+              break;
+              case 'b':
+                print("Blue", value);
+                nextBlue = value.toInt();
+              break;
+              case '#':
+                Serial.println("Hex String ");
+                Serial.print("r ");
+                nextRed = strtoul(value.substring(0,2).c_str(), 0, 16);
+                Serial.println(nextRed);
+                Serial.print("g ");
+                nextGreen = strtoul(value.substring(2,4).c_str(), 0, 16);
+                Serial.println(nextGreen);
+                Serial.print("b ");
+                nextBlue = strtoul(value.substring(4,6).c_str(), 0, 16);
+                Serial.println(nextBlue);
+              break;
+              default:
+                Serial.print("I received: ");
+                Serial.print((char)incomingByte);
+                Serial.print(" ");
+                Serial.println(value);
+            }
+    }
+}
+
+void print(String color, String value) {
+    Serial.print("I received: ");
+    Serial.print(color);
+    Serial.print(' ');
+    Serial.println(value);
+}
+
+void changeColors() {
+    colorMod = colorMod + 1;
+    if (colorMod > maxColorMod) {
+        showColors = !showColors;
+        colorMod = 0;
+    }
+
+    if (showColors) {
+        redA = updateColor(nextRed, redA, redLed);
+        greenA = updateColor(nextGreen, greenA, greenLed);
+        blueA = updateColor(nextBlue, blueA, blueLed);
+    } else {
+        redA = updateColor(0, redA, redLed);
+        greenA = updateColor(0, greenA, greenLed);
+        blueA = updateColor(0, blueA, blueLed);
+    }
+}
+
+int updateColor(int nextColor, int actualColor, int ledPin) {
+    if (nextColor > actualColor)
+        actualColor = actualColor + 1;
+    else if (nextColor < actualColor)
+        actualColor = actualColor - 1;
+    analogWrite(ledPin, actualColor);
+    return actualColor;
+}
